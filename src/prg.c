e639458b5dc3d493325a2bac05eb3aa04bc732c2
dev <46674147+dev7060@users.noreply.github.com>
2021-03-19 23:46:34 +0530

    Update prg.c

--- src/prg.c
+++ src/prg.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <conio.h>
+#include <time.h>
 int main(){
 	system("cls");
+	system("TITLE Clock by github.com/dev7060");
+	system("COLOR 0A");
+	int hours, minutes, seconds;
+	while(1){
+		time_t now;
+		time(&now);
+		struct tm *local = localtime(&now);
+    	hours = local->tm_hour;
+    	minutes = local->tm_min;
+    	seconds = local->tm_sec;
+    	system("cls");
+    	printf("\n\n\n\n\n\n\n\n\n\n\n");
+    	printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t%d:%d:%d\n\n\n\n\n\n", hours, minutes, seconds);
+		printf("\n\n\n\n\n\n\n\n\n\n");
+	}
+	
+	system("pause");
 	return 0;
 }
